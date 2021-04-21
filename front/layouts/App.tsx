ef2d88e576ce3ff3efb0dff6a59c9406d7d7a13f
WooDong <tmlu48@naver.com>
2021-04-22 04:19:25 +0900

    chore: 프로젝트 프론트 엔드 환경 설정

---
+++ front/layouts/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import loable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const Login = loable(() => import('@pages/Login'));
+const SignUp = loable(() => import('@pages/SignUp'));
+
+const App = () => {
+  return (
+    //Switch : 여러개 중 하나만 선택하는 라우터 ( 아래 3개중 하나만 선택해서 컴포넌트를 보여주게됩니다. 라우터로 감싸줘야 합니다.
+    //Redirect : 다른페이지로 돌려주는 역할, 주소가 /로 끝나면 /login으로 옮겨줍니다.
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
