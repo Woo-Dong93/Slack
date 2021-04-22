3d481eef8ed351a88288ab97d706b0e31a727d7b
WooDong <tmlu48@naver.com>
2021-04-22 21:23:56 +0900

    feat: 회원가입 비동기 API요청을 통한 기능 완성

--- front/hooks/useInput.ts
+++ front/hooks/useInput.ts
@@ -1,8 +1,8 @@
 import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 
-type ReturnTypes<T = any> = [T, (e: React.ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
+type ReturnTypes<T> = [T, (e: React.ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
 
-const useInput = <T = any>(initialData: T): ReturnTypes<T> => {
+const useInput = <T>(initialData: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialData);
 
   const handler = useCallback((e) => {
