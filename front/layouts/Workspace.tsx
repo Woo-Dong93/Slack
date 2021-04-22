a02650f9d87f8846fe481aac88dfc6f2300aa6a1
WooDong <tmlu48@naver.com>
2021-04-23 01:10:40 +0900

    update: 로그인하기 기능에 SWR 설정

---
+++ front/layouts/Workspace.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Workspace = () => {
+  return <button>로그아웃</button>;
+};
+
+export default Workspace;
---
+++ front/pages/Channel/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Channel = () => {
+  return <div>로그인하신 것을 축하드려요!</div>;
+};
+
+export default Channel;
