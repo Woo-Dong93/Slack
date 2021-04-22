2ab94b94d98a1b936d2108c94ff3939bb2886c0c
WooDong <tmlu48@naver.com>
2021-04-23 01:44:38 +0900

    update: 로그인아웃 구현 및 접근권한 설정

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,7 +1,12 @@
+import Workspace from '@layouts/Workspace';
 import React from 'react';
 
 const Channel = () => {
-  return <div>로그인하신 것을 축하드려요!</div>;
+  return (
+    <Workspace>
+      <div>로그인하신 것을 축하드려요!</div>
+    </Workspace>
+  );
 };
 
 export default Channel;
