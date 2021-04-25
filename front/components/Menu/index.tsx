802aab450219e049e00278fe1fab4ed1af3565cf
WooDong <tmlu48@naver.com>
2021-04-25 20:33:53 +0900

    update: 계정 클릭시 모달 기능 구현

---
+++ front/components/Menu/index.tsx
@@ -0,0 +1,32 @@
+import { CloseModalButton } from '@components/Menu/styles';
+import React, { CSSProperties, FC, useCallback } from 'react';
+import { CreateMenu } from './styles';
+
+interface Props {
+  show: boolean;
+  onCloseModal: (e: any) => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<Props> = ({ children, style, show, onCloseModal, closeButton }) => {
+  const stopPropaggation = useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div style={style} onClick={stopPropaggation}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+
+// 부모에서 프롭스를 안넘겨주면 기본값으로 설정가능!
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
---
+++ front/components/Modal/index.tsx
@@ -0,0 +1,22 @@
+// import { CloseModalButton } from '@components/Menu/styles';
+// import React, { CSSProperties, FC } from 'react';
+// import { CreateMenu } from './styles';
+
+// interface Props {
+//   show: boolean;
+//   onCloseModal: () => void;
+//   style: CSSProperties;
+//   closeButton?: boolean;
+// }
+
+// const Menu: FC<Props> = ({ children, style, show, onCloseModal, closeButton }) => {
+//   return (
+//     <CreateMenu onClick={onCloseModal}>
+//       <div style={style}>메뉴</div>
+//       {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+//       {children}
+//     </CreateMenu>
+//   );
+// };
+
+// export default Menu;
