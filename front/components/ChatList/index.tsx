cc7c3418f8aabaa28db8c9bb94160675910a2679
WooDong <tmlu48@naver.com>
2021-04-28 04:58:08 +0900

    update: 리버스 무한 스크롤 구현하기

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,20 +1,29 @@
 import Chat from '@components/Chat';
 import { IDM } from '@typings/db';
-import React, { useCallback, useRef, VFC } from 'react';
+import React, { useCallback, useRef, VFC, forwardRef } from 'react';
 import { ChatZone, Section, StickyHeader } from './styles';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatSections: { [key: string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
 
-const ChatList: VFC<Props> = ({ chatSections }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }, ref) => {
+  const onScroll = useCallback((values) => {
+    if (values.scrollTop === 0 && !isReachingEnd) {
+      // 과거데이터를 기반으로 갱신한다.
+      setSize((prevSize) => prevSize + 1).then(() => {
+        //스크롤 위치 유지
+      });
+    }
+  }, []);
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chats]) => {
           return (
             <Section className={`secton-${date}`} key={date}>
@@ -30,6 +39,6 @@ const ChatList: VFC<Props> = ({ chatSections }) => {
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
