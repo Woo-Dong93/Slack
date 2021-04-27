13bd3da8eaa23139d87bc4d1291e3d554f186160
WooDong <tmlu48@naver.com>
2021-04-28 01:44:08 +0900

    refactor: memtions 기능 양식 변경 및 최적화

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,15 +1,47 @@
 import { IDM } from '@typings/db';
-import React, { VFC } from 'react';
+import React, { VFC, memo, useMemo } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string }>();
   const user = data.Sender;
+  // 정규표현식 : //g:모두찾겠다. \ : 특수기호를 무력화(특정한 역할 무력화), .+? : 모든 문자를 1개이상, \d+ : 숫자 1자리 이상
+  // \d : 숫자
+  // + : 1개 이상
+  // ? : 0개나 1개
+  // g : 모두찾기
+  // +? : 1개이상이면서 최대한 조금 찾기
+  // | : 또는
+  // \n : 줄바꿈
+  // @[이름](id)
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          //id에 걸리는게 있으면
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,10 +52,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
