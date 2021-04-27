5237feeaa235b30f56b65c3016c14a5c8e6e0099
WooDong <tmlu48@naver.com>
2021-04-28 04:11:45 +0900

    refactor: 채팅 내역 날짜별로 그룹화

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -9,6 +9,7 @@ import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import { IDM } from '@typings/db';
+import makeSection from '@utils/makeSection';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
@@ -48,13 +49,16 @@ const DirectMessage = () => {
     return null;
   }
 
+  // 그냥 reverse()는 기존 배열이 바뀌어버린다. (immutable하지 않다.)
+  const chatSections = makeSection(chatData ? [...chatData].reverse() : []);
+
   return (
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatData={chatData} />
+      <ChatList chatSections={chatSections} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
